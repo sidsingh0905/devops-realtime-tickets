aab07d2908195b488add353dd01472fba7fc6181
Shiva Ashish <sidsingh0905@gmail.com>
2025-12-29 20:22:03 +0530

    Ticket 02: Baseline private EC2 without internet access

---
+++ terraform/ticket-02-nat-gateway/main.tf
@@ -0,0 +1,109 @@
+resource "aws_vpc" "dev_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "dev-vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.dev_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "dev-public-subnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = aws_vpc.dev_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "dev-private-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.dev_vpc.id
+
+  tags = {
+    Name = "dev-igw"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.dev_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+    tags = {
+        Name = "dev-public-rt"
+    }
+}
+
+resource "aws_route_table_association" "public_rt_assoc" {
+      subnet_id      = aws_subnet.public_subnet.id
+      route_table_id = aws_route_table.public_rt.id
+}
+
+
+
+resource "aws_security_group" "private_ec2_sg" {
+  name   = "private-ec2-sg"
+  vpc_id = aws_vpc.dev_vpc.id
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    cidr_blocks = [ "49.36.169.67/32" ]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "private-ec2-sg"
+  }
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"]
+}
+
+resource "aws_instance" "private_ec2" {
+  ami                         = data.aws_ami.amazon_linux.id 
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.private_subnet.id
+  associate_public_ip_address = false
+  vpc_security_group_ids      = [aws_security_group.private_ec2_sg.id]
+  key_name                    = "devops-bastion-key"
+
+  tags = {
+    Name = "dev-private-ec2"
+  }
+}   
